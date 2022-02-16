9d18636ebe9471f6cc2fa63b891c9fb4f3ee443a
Treefire33 <63606230+Treefire33@users.noreply.github.com>
2022-02-16 09:07:00 -0600

    Main webpage files

---
+++ main.js
@@ -0,0 +1,10 @@
+var layers = document.getElementById("layers");
+var generateButton = document.getElementById("generate");
+
+generateButton.onclick = generateLayers;
+
+function generateLayers()
+{
+    
+    layers.innerText = "Layers: " + Math.floor(Math.random() * 11);
+}
