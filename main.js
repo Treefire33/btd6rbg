735ad277f78adb3d1417b39d6b70f4ecc338581f
Treefire33 <braidonaksingh@gmail.com>
2022-02-17 18:02:02 -0600

    organization and updated code

--- main.js
+++ main.js
@@ -13,6 +13,8 @@ usesHealthCheck.onclick = setUsesHealth;
 var genedLayers = getRandomInt(1,8);
 var genedHealth;
 
+generateLayers();
+
 function generateLayers()
 {  
     genedLayers = getRandomInt(1,8);
@@ -49,37 +51,37 @@ function getBloonAlike(layersOnBloon)
     {
         case 1:
             bloonAlikes.innerHTML = 'Bloon-Alike: <br/> Red Bloon';
-            bloonImgs.innerHTML = '<img src="BTD6Red.png"/>';
+            bloonImgs.innerHTML = '<img src="imgs/BTD6Red.png"/>';
         break;
         case 2:
             bloonAlikes.innerHTML = 'Bloon-Alike: <br/> Blue Bloon';
-            bloonImgs.innerHTML = '<img src="BTD6Blue.png"/>';
+            bloonImgs.innerHTML = '<img src="imgs/BTD6Blue.png"/>';
         break;
         case 3:
             bloonAlikes.innerHTML = 'Bloon-Alike: <br/> Green Bloon';
-            bloonImgs.innerHTML = '<img src="BTD6Green.png"/>';
+            bloonImgs.innerHTML = '<img src="imgs/BTD6Green.png"/>';
         break;
         case 4:
             bloonAlikes.innerHTML = 'Bloon-Alike: <br/> Yellow Bloon';
-            bloonImgs.innerHTML = '<img src="BTD6Yellow.png"/>';
+            bloonImgs.innerHTML = '<img src="imgs/BTD6Yellow.png"/>';
         break;
         case 5:
             bloonAlikes.innerHTML = 'Bloon-Alike: <br/> Pink Bloon';
-            bloonImgs.innerHTML = '<img src="BTD6Pink.png"/>';
+            bloonImgs.innerHTML = '<img src="imgs/BTD6Pink.png"/>';
         break;
         case 6:
             bloonAlikes.innerHTML = 'Bloon-Alike: <br/> Black, White or Purple Bloon';
-            bloonImgs.innerHTML = '<img src="BTD6Black.png"/>';
-            bloonImgs.innerHTML += '<img src="BTD6White.png"/>';
-            bloonImgs.innerHTML += '<img src="Purplebloon.png"/>';
+            bloonImgs.innerHTML = '<img src="imgs/BTD6Black.png"/>';
+            bloonImgs.innerHTML += '<img src="imgs/BTD6White.png"/>';
+            bloonImgs.innerHTML += '<img src="imgs/Purplebloon.png"/>';
         break;
         case 7:
             bloonAlikes.innerHTML = 'Bloon-Alike: <br/> Zebra/Striped Bloon';
-            bloonImgs.innerHTML = '<img src="BTD6Zebra.png"/>';
+            bloonImgs.innerHTML = '<img src="imgs/BTD6Zebra.png"/>';
         break;
         case 8:
             bloonAlikes.innerHTML = 'Bloon-Alike: <br/> Rainbow Bloon';
-            bloonImgs.innerHTML = '<img src="BTD6Rainbow.png"/>';
+            bloonImgs.innerHTML = '<img src="imgs/BTD6Rainbow.png"/>';
         break;
     }
 }
@@ -89,32 +91,32 @@ function getMoabALike()
     if(genedHealth >= 1 && genedHealth < 200)
     {
         bloonAlikes.innerHTML = 'M.O.A.B-Alike: <br/> Ceramic Bloon';
-        bloonImgs.innerHTML = '<img src="BTD6Ceramic.png"/>';
+        bloonImgs.innerHTML = '<img src="imgs/BTD6Ceramic.png"/>';
     }
     if(genedHealth >= 200 && genedHealth < 400)
     {
         bloonAlikes.innerHTML = 'M.O.A.B-Alike: <br/> M.O.A.B';
-        bloonImgs.innerHTML = '<img src="BTD6_MOAB_Artwork.png"/>';
+        bloonImgs.innerHTML = '<img src="imgs/BTD6_MOAB_Artwork.png"/>';
     }
     if(genedHealth >= 400 && genedHealth < 700)
     {
         bloonAlikes.innerHTML = 'M.O.A.B-Alike: <br/> D.D.T';
-        bloonImgs.innerHTML = '<img src="BTD6_DDT_Artwork.png"/>';
+        bloonImgs.innerHTML = '<img src="imgs/BTD6_DDT_Artwork.png"/>';
     }
     if(genedHealth >= 700 && genedHealth < 4000)
     {
         bloonAlikes.innerHTML = 'M.O.A.B-Alike: <br/> B.F.B';
-        bloonImgs.innerHTML = '<img src="BTD6_BFB_Artwork.png"/>';
+        bloonImgs.innerHTML = '<img src="imgs/BTD6_BFB_Artwork.png"/>';
     }
     if(genedHealth >= 4000 && genedHealth < 20000)
     {
         bloonAlikes.innerHTML = 'M.O.A.B-Alike: <br/> Z.O.M.G';
-        bloonImgs.innerHTML = '<img src="BTD6_ZOMG_Artwork.png"/>';
+        bloonImgs.innerHTML = '<img src="imgs/BTD6_ZOMG_Artwork.png"/>';
     }
     if(genedHealth >= 20000)
     {
         bloonAlikes.innerHTML = 'M.O.A.B-Alike: <br/> B.A.D';
-        bloonImgs.innerHTML = '<img src="BTD6_BAD_Artwork.png"/>';
+        bloonImgs.innerHTML = '<img src="imgs/BTD6_BAD_Artwork.png"/>';
     }
 }
 
