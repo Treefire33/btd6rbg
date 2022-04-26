4e17f2e9052d5bdbcad15886561335250a7d62bb
Braidon <63606230+Treefire33@users.noreply.github.com>
2022-04-26 11:27:25 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,9 +27,9 @@ function setVisitText()
 {
     if(window.localStorage.getItem("visits") >= 1)
     {
-        let n = window.localStorage.getItem("visits")+1;
+        let n = Number(window.localStorage.getItem("visits"))+1;
         alert(n);
-        window.localStorage.setItem("visits", n);
+        window.localStorage.setItem("visits", n.toString());
         valueText.innerText = "You have visited this website: "+window.localStorage.getItem("visits")+" times";
     }
     else
